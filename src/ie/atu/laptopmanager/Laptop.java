376513dea227927c0da1888b640be8c02499ec39
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-11-21 16:19:23 +0000

    fourth change

--- src/ie/atu/laptopmanager/Laptop.java
+++ src/ie/atu/laptopmanager/Laptop.java
@@ -4,7 +4,40 @@ public class Laptop {
 
     // Instance Variables
     private int serialNumber; // Unique ID
-    private String brand;
-    private float price;
-    private boolean isSold;
+    private int brand;
+    private int price;
+    private int isSold;
+
+    // Getter and Setter Methods
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
+
+    public int getBrand() {
+        return this.brand;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public int getIsSold() {
+        return this.isSold;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setBrand(int brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setisSold(int isSold) {
+        this.isSold = isSold;
+    }
 }
