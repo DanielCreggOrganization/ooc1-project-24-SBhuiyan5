0eb603d901bd7f07daeb0f7d87c958daa7443b6f
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-11-21 16:01:28 +0000

    third change

--- src/ie/atu/laptopmanager/Laptop.java
+++ src/ie/atu/laptopmanager/Laptop.java
@@ -2,4 +2,9 @@ package ie.atu.laptopmanager;
 
 public class Laptop {
 
+    // Instance Variables
+    private int serialNumber; // Unique ID
+    private String brand;
+    private float price;
+    private boolean isSold;
 }
