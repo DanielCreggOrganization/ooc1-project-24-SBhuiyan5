5ffb36d2834a9d967128537993097a093793bc2a
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-11-21 15:46:45 +0000

    first layout

---
+++ src/ie/atu/laptop/Laptop.java
@@ -0,0 +1,5 @@
+package ie.atu.laptop;
+
+public class Laptop {
+
+}
---
+++ src/ie/atu/laptop/Main.java
@@ -0,0 +1,10 @@
+package ie.atu.laptop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow there!");
+
+    }
+
+}
