94638211279cce0308cd305446e4ee6a5db06f40
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-12-01 16:58:41 +0000

    updated change for menu

--- src/ie/atu/laptopmanager/Main.java
+++ src/ie/atu/laptopmanager/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner userInput = a new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         List<Laptop> laptops = new ArrayList<>();
         int userSelection = 0;
 
@@ -20,23 +20,32 @@ public class Main {
         System.out.println("####################################");
         System.out.println("#         Laptop Application v0.9  #");
         System.out.println("####################################");
-        System.out.println("(1) Add a Student.");
-        System.out.println("(2) Delete a Student.");
-        System.out.println("(3)Show total Number of Students.");
-        System.out.println("(4)Search for a Student by ID.");
+        System.out.println("(1) Add a Laptop.");
+        System.out.println("(2) Delete a Laptop.");
+        System.out.println("(3)Show total Laptops by Serial Number.");
+        System.out.println("(4)Search for a Laptop by Serial Number.");
         System.out.println("(5) Quit.");
         System.out.println("Select an option from 1 to 5 and press Enter.");
 
         // Store user Menu option selection
         userSelection = userInput.nextInt();
 
-        if (userSelection == 5) { // Quit
-
-            System.out.println("Laptop Application Closing - Goodbye!");
-            // Close the User Input Scanner
+        if (userSelection == 1) { 
+            addLaptop(userInput, laptops);
+        }   else if (userSelection == 2) {
+            deleteLaptop(userInput, laptops);
+        }   else if (userSelection == 3) {
+            showTotalLaptop(laptops);
+        }   else if (userSelection == 4) {   
+            searchLaptop(userInput, laptops);
+        }   else if (userSelection == 5) {
+            System.out.println("The Application Is Now Closing, Thank You!") // Close the User Input Scanner
             userInput.close();
             break; // Break out of display menu infinite loop
-        } // end it
-    } // end while
-  } // end main method
-} // end Main class
+        }   else {
+            System.out.println("Invalid! Please Select The Options From 1 to 5, Thank You!");
+                } // end it    
+            } // end while
+        } // end main method
+
+     } // end main class
