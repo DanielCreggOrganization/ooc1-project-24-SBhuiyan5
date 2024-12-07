700386d05947cba303e825fc9f7c24eb376c2f7a
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-12-07 14:49:24 +0000

    comments added to laptop.java

--- src/ie/atu/laptopmanager/Laptop.java
+++ src/ie/atu/laptopmanager/Laptop.java
@@ -1,21 +1,22 @@
-package ie.atu.laptopmanager;
+package ie.atu.laptopmanager; // package declaration
 
-public class Laptop {
+public class Laptop { // class declaration
 
     // Instance Variables
     private int serialNumber; // Unique ID
-    private String brand;
-    private float price;
-    private boolean isSold;
+    private String brand; // Brand Name
+    private float price; // Laptop Price
+    private boolean isSold; // How Much Is Sold Or Not
 
     // Constructor
     public Laptop(int serialNumber, String brand, double price, boolean isSold){
-        this.serialNumber = serialNumber;
-        this.brand = brand;
-        this.price = (float) price;
-        this.isSold = isSold;
+        this.serialNumber = serialNumber; // Assigns Serial Number to Instance Variable (int)
+        this.brand = brand; // Assigns Brand to Instance Variable (String)
+        this.price = (float) price; // Changes "Price" from double to float 
+        this.isSold = isSold; // Assigns isSold to Instance Variable (boolean)
     }
 
+// Getter and Setter Methods
 public int getSerialNumber() {
     return this.serialNumber;
 }
