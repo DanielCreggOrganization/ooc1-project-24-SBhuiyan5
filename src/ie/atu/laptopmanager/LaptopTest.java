604388e543021166a98a865b60d4774b4482d4a7
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-12-07 14:57:41 +0000

    comments added to LaptopTest.java

--- src/ie/atu/laptopmanager/LaptopTest.java
+++ src/ie/atu/laptopmanager/LaptopTest.java
@@ -1,14 +1,16 @@
-package ie.atu.laptopmanager;
+package ie.atu.laptopmanager; // package declaration
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; // imports ArrayList class to create Arrays
+import java.util.List; // imports list interface for functionality
 
-public class LaptopTest {
-    public static void main(String[] args) {
-        List<Laptop> testLaptops = new ArrayList<>();
-        testLaptops.add(new Laptop(101, "Asus", 500.00, false));
+public class LaptopTest { // class declaration
+    public static void main(String[] args) { // main method
+        List<Laptop> testLaptops = new ArrayList<>(); // list created to store laptop objects
+        // new laptop objects added to instance variables
+        testLaptops.add(new Laptop(101, "Asus", 500.00, false)); 
         testLaptops.add(new Laptop(102, "HP", 300.00, true));
 
+        // prints total number of laptops within the list
         System.out.println("Total Number of Laptops: " + testLaptops.size());
     }
 
