89aa43683cb613a256ea7221e3b84e8615393ad4
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-12-15 16:04:17 +0000

    added new base class device.java

---
+++ src/ie/atu/laptopmanager/Device.java
@@ -0,0 +1,39 @@
+package ie.atu.laptopmanager; // package declaration
+
+public class Device { // class declaration
+    private int serialNumber; // Unique ID
+    private String brand; // brand name
+    private float price; // laptop price
+
+    // constructor
+    public Device(int serialNumber, String brand, float price) {
+        this.serialNumber = serialNumber; // Assigns Serial Number to Instance Variable (int)
+        this.brand = brand; // Assigns Brand to Instance Variable (String)
+        this.price = (float) price; // Changes "Price" from double to float 
+    }
+
+// Getter and Setter Methods
+public int getSerialNumber() {
+    return this.serialNumber;
+}
+
+public void setSerialNumber(int serialNumber) {
+    this.serialNumber = serialNumber;
+}
+
+public String getBrand() {
+    return brand;
+}
+
+public void setBrand(String brand) {
+    this.brand = brand;
+}
+
+public float getPrice() {
+    return price;
+}
+
+public void setPrice(float price) {
+    this.price = price;
+}
+}
