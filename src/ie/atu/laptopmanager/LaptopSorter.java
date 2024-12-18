8738c4cefb29fb5c50ad2c3dc9e4e30bec2c1101
SBhuiyan5 <114398377+SBhuiyan5@users.noreply.github.com>
2024-12-18 15:25:21 +0000

    created laptop sorter class

---
+++ src/ie/atu/laptopmanager/LaptopSorter.java
@@ -0,0 +1,33 @@
+package ie.atu.laptopmanager; // package declaration
+
+import java.util.ArrayList; // imports ArrayList class to create Arrays
+import java.util.Comparator; // imports 
+import java.util.Collections; // imports 
+
+public class LaptopSorter {
+    public static void sortByBrand(ArrayList<Laptop> laptops) {
+        if (laptop.isEmpty()) {
+            System.out.println("No Laptops Sorted by Brand")
+            return;
+        }
+
+        Collections.sort(laptops, Comparator.comparing(Laptop::getBrand));
+        System.out.println("Laptops Sorted by Brand: ");
+        displayLaptops(laptops);
+    }
+    public static void sortByPrice(ArrayList<Laptop> laptops) {
+        if (laptop.isEmpty()) {
+            System.out.println("No Laptops Sorted by Brand")
+            return;
+        }
+
+        Collections.sort(laptops, Comparator.comparing(Laptop::getPrice));
+        System.out.println("Laptops Sorted by Price: ");
+        displayLaptops(laptops);
+    }
+    private static void displayLaptops(ArrayList<Laptop> laptops) {
+        for (Laptop laptop : laptops) {
+            System.out.println(laptop);
+        }
+    }
+}
